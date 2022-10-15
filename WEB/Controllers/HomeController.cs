b75187028be422b017de39d582f2e340d2bab63a
maritogram <94982527+maritogram@users.noreply.github.com>
2022-10-15 03:13:22 -0400

    Log4Net y transactions ready para webapp.

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -7,7 +8,14 @@ namespace WEB.Controllers
 {
     public class HomeController : Controller
     {
+        //init entitiy framework 
         DtEntities entities = new DtEntities();
+
+
+        //log4net init
+        private static readonly ILog Logger = LogManager.GetLogger(System.Environment.MachineName);
+
+
 
         //soap clients
         integracionSR.integracionSWSoapClient clientIntegracion = new integracionSR.integracionSWSoapClient();
@@ -65,7 +73,7 @@ namespace WEB.Controllers
                 }
             }
 
-            ViewData["Error"] = "Célula no registrada";
+            ViewData["Error"] = "Cédula no registrada";
             return View();
         }
 
@@ -85,11 +93,32 @@ namespace WEB.Controllers
             var clienteEncontrado = entities.spSelectByCedu(cliente.Cedula).SingleOrDefault();
             if (clienteEncontrado == null)
             {
-                entities.spInsCliente(cliente.Nombres, cliente.Apellidos, cliente.Cedula, cliente.Telefono, cliente.fechaNacimiento, cliente.Email, cliente.Password, cliente.Sexo);
-                clientIntegracion.InsertClienteINTEGRACION(cliente.Nombres, cliente.Apellidos, cliente.Cedula, cliente.Telefono, cliente.fechaNacimiento, cliente.Email, cliente.Password, cliente.Sexo);
-                return RedirectToAction("Login");
+                using (var dbContextTransaction = entities.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        entities.spInsCliente(cliente.Nombres, cliente.Apellidos, cliente.Cedula, cliente.Telefono, cliente.fechaNacimiento, cliente.Email, cliente.Password, cliente.Sexo);
+
+                        clientIntegracion.InsertClienteINTEGRACION(cliente.Nombres, cliente.Apellidos, cliente.Cedula, cliente.Telefono, cliente.fechaNacimiento, cliente.Email, cliente.Password, cliente.Sexo);
+
+                        
+                        Logger.Info("Cliente " + cliente.Nombres + " fue insertado.");
+                        dbContextTransaction.Commit();
+                        return RedirectToAction("Login");
+                    }
+                    catch (Exception)
+                    {
+                        Logger.Error("Error al crear registro. Intente de nuevo.");
+                        ViewData["Error"] = "Error al crear registro. Intente de nuevo.";
+                        dbContextTransaction.Rollback();
+                        return View();
+
+                    }
+
+                }
             }
 
+            Logger.Warn("Error al crear registro.");
             ViewData["Error"] = "Cliente ya existe";
 
             return View();
@@ -102,13 +131,31 @@ namespace WEB.Controllers
                 Session["Usuario"] = "ANONIMO";
 
             var producto = entities.spSelectProductoByCodigo(productoCodigo).Single();
-            entities.spInsertProductoACarrito(producto.codigo, producto.tipo, producto.marca, producto.precio, producto.cantidad, producto.nombre, producto.peso, producto.imagen, producto.descripcion, Session["Usuario"].ToString());
 
-            clientIntegracion.InsertarCarritoINTEGRACION(producto.codigo, producto.marca, producto.precio, producto.cantidad,  producto.tipo, producto.nombre, (int)producto.peso, producto.imagen, producto.descripcion, Session["Usuario"].ToString());
+            using (var dbContextTransaction = entities.Database.BeginTransaction())
+            {
+                try
+                {
+                    entities.spInsertProductoACarrito(producto.codigo, producto.tipo, producto.marca, producto.precio, producto.cantidad, producto.nombre, producto.peso, producto.imagen, producto.descripcion, Session["Usuario"].ToString());
 
-            entities.spUpdateProductoEstado(productoCodigo, Session["Usuario"].ToString());
+                    clientIntegracion.InsertarCarritoINTEGRACION(producto.codigo, producto.marca, producto.precio, producto.cantidad, producto.tipo, producto.nombre, (int)producto.peso, producto.imagen, producto.descripcion, Session["Usuario"].ToString());
 
-            return RedirectToAction("Carrito");
+                    entities.spUpdateProductoEstado(productoCodigo, Session["Usuario"].ToString());
+
+                    Logger.Info("Cliente con cedula " + Session["Usuario"].ToString() + " agrego el producto " + producto.nombre + " al carrito.");
+                    dbContextTransaction.Commit();
+                    return RedirectToAction("Carrito");
+                }
+                catch (Exception)
+                {
+                    ViewData["Error"] = "Error con la conexion. Producto no insertado al carrito.";
+                    Logger.Error("Error con la conexion. Producto no insertado al carrito.");
+                    dbContextTransaction.Rollback();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            
         }
 
         [Route("Carrito")]
@@ -148,7 +195,7 @@ namespace WEB.Controllers
             return View();
         }
 
-
+       
         [HttpPost]
         public ActionResult CarritoComprar()
         {
@@ -173,31 +220,57 @@ namespace WEB.Controllers
             {
                 string cuentaID = myuuid.ToString();
 
-                if (clienteEncontrado == null)
+                using (var dbContextTransaction = entities.Database.BeginTransaction())
                 {
-                    entities.spInsCuenta("ANONIMO", "ANONIMO", item.nombre, item.precio, cuentaID, "ANONIMO");
+                    try
+                    {
 
-                    clientIntegracion.InsertCuentaINTEGRACION("ANONIMO", "ANONIMO", item.nombre, item.precio, cuentaID, "ANONIMO");
+                        if (clienteEncontrado == null)
+                        {
 
-                    entities.spDelCarrito("ANONIMO");
 
-                    clientIntegracion.DeleteCarritoINTEGRACION("ANONIMO");
+                            entities.spInsCuenta("ANONIMO", "ANONIMO", item.nombre, item.precio, cuentaID, "ANONIMO");
 
-                    entities.spUpdateProductoEstado(item.codigo, null);
+                            clientIntegracion.InsertCuentaINTEGRACION("ANONIMO", "ANONIMO", item.nombre, item.precio, cuentaID, "ANONIMO");
+
+                            entities.spDelCarrito("ANONIMO");
+
+                            clientIntegracion.DeleteCarritoINTEGRACION("ANONIMO");
+
+                            entities.spUpdateProductoEstado(item.codigo, null);
+
+                        }
+                        else
+                        {
+                            entities.spInsCuenta(clienteEncontrado.Nombres, clienteEncontrado.Apellidos, item.nombre, item.precio, cuentaID, Session["Usuario"].ToString());
+
+                            clientIntegracion.InsertCuentaINTEGRACION(clienteEncontrado.Nombres, clienteEncontrado.Apellidos, item.nombre, item.precio, cuentaID, Session["Usuario"].ToString());
+
+                            entities.spDelCarrito(Session["Usuario"].ToString());
+
+                            clientIntegracion.DeleteCarritoINTEGRACION(Session["Usuario"].ToString());
+
+                            entities.spUpdateProductoEstado(item.codigo, null);
+                        }
+
+                        Logger.Info("Cuenta de " + Session["Usuario"].ToString() + " esta pendiente para pagar.");
+                        dbContextTransaction.Commit();
+
+
+                    }
+                    catch (Exception)
+                    {
+
+                        Logger.Info("Error al procesar pago.");
+                        dbContextTransaction.Rollback();
+                        return RedirectToAction("Carrito");
+
+                    }
+
                 }
-                else 
-                {
-                    entities.spInsCuenta(clienteEncontrado.Nombres, clienteEncontrado.Apellidos, item.nombre, item.precio, cuentaID, Session["Usuario"].ToString());
 
-                    clientIntegracion.InsertCuentaINTEGRACION(clienteEncontrado.Nombres, clienteEncontrado.Apellidos, item.nombre, item.precio, cuentaID, Session["Usuario"].ToString());
 
-                    entities.spDelCarrito(Session["Usuario"].ToString());
 
-                    clientIntegracion.DeleteCarritoINTEGRACION(Session["Usuario"].ToString());
-
-                    entities.spUpdateProductoEstado(item.codigo, null);
-                }
-                   
             }
 
             return RedirectToAction("CuentasPorCobrar");
